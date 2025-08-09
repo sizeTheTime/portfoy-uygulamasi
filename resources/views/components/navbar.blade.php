43dc780698eca587d40623d934c1a74245aa24be
sizeTheTime <ikateknolojin11@gmail.com>
2025-08-10 01:14:23 +0300

    Blog sayfası hatası düzeltildi.

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -109,8 +109,9 @@
                 @else
                     {{-- Empty State --}}
                     <div class="px-4 py-8 text-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
+
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-12 text-gray-400 mx-auto mb-3">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
                         </svg>
 
                         <p class="text-sm text-gray-500 normal-case tracking-normal">Henüz kategori bulunmuyor</p>
