ac466cc64f63c3907973eda08c762e34e70630bc
sizeTheTime <ikateknolojin11@gmail.com>
2025-08-10 22:44:53 +0300

    Mobil ui Düzenlemesi yapıldı.

--- resources/js/components/ui/table/TableRow.vue
+++ resources/js/components/ui/table/TableRow.vue
@@ -20,9 +20,9 @@ const handleClick = (event: MouseEvent) => {
 </script>
 
 <template>
-  <tr 
+  <tr
     :class="cn(
-      'border-b border-gray-200 dark:border-gray-700 transition-colors',
+      'border-b border-gray-200 dark:border-gray-700 transition-colors max-w-sm',
       {
         'hover:bg-gray-50 dark:hover:bg-gray-800/50 cursor-pointer': clickable,
         'bg-blue-50 dark:bg-blue-950/20 border-blue-200 dark:border-blue-800': selected,
